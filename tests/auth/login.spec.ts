e89c02547c14ff228ee5d28f11fe34db485fe1fb
mattb230 <mattb230@gmail.com>
2026-03-30 01:45:38 -0500

    Initial commit

---
+++ tests/auth/login.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test'
+import { LoginPage } from '../../pages/LoginPage'
+import { users } from '../../test-data/users'
+
+test.describe('Login', () => {
+
+  test('standard user can log in successfully', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    await loginPage.goto()
+    await loginPage.login(users.standard.username, users.standard.password)
+    await expect(page).toHaveURL(/inventory/)
+  })
+
+  test('locked out user sees error message', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+    await loginPage.goto()
+    await loginPage.login(users.locked.username, users.locked.password)
+    await expect(loginPage.errorMessage).toBeVisible()
+  })
+
+})
